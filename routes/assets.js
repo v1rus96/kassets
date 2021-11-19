6bee57c51faff0ae218ace2909d5af556a4c3f92
Firuz <firuz.akhmadov@gmail.com>
2021-11-20 06:44:18 +0800

    shorturl

--- routes/assets.js
+++ routes/assets.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const Image = require("../models/image");
+const shortid = require("shortid");
 
 
 router.get('/upload', (req, res) => res.render('upload'));
 router.post("/upload", (req, res, next) => {
   const { url, category, name } = req.body;
   const image = new Image({
-    imageID: "1",
+    imageID: shortid.generate(),
     name: name,
     category: category,
     url: url
